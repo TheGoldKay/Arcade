baef846df40e136c0ca7f71ab3121994afcf4739
jhonnatan carvalho <jhonnatanwfc@live.com>
2024-01-04 08:37:30 -0300

    Sprite to (row, col) Grid

--- snake/main.py
+++ snake/main.py
@@ -3,23 +3,35 @@ import snake
 
 class GameScreen(arcade.Window):
     def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+        super().__init__(width, height, title, center_window=True)
+        self.background_color = (18, 53, 36)  # phthalo green
 
     def setup(self):
+        self.snake = snake.Snake()
+        self.grid = self.get_grid()
+    
+    def get_grid(self):
         pass
-
+    
+    def draw_grid(self):
+        pass
+    
     def on_draw(self):
+        self.clear()
+        self.draw_grid()
+        self.snake.draw()
+    
+    def update_grid(self):
         pass
 
     def on_update(self, delta_time):
-        pass
+        self.snake.update(delta_time)
+        self.update_grid()
 
     def on_key_press(self, key, modifiers):
-        pass
-
-    def on_key_release(self, key, modifiers):
-        pass
-
+        if key == arcade.key.ESCAPE:
+            arcade.close_window()
+            
 def main():
     window = GameScreen(800, 600, "Snake Game")
     window.setup()
--- snake/snake.py
+++ snake/snake.py
@@ -1,4 +1,11 @@
 import arcade 
 
-class Snake(arcade.Sprite):
-    pass
+class Snake:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+    
+    def update(self, dt):
+        pass
