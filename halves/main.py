6a7d8f66c5bea5f8bacdae49553e7c614de0b60a
GoldKay <jhonnatanwfc@live.com>
2024-12-18 16:07:47 -0300

    fix coordinate system

--- halves/main.py
+++ halves/main.py
@@ -3,9 +3,10 @@ import sys
 
 # Constants
 SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 500
+SCREEN_HEIGHT = 400
 SCREEN_TITLE = "Halves"
 BACKGROUND_COLOR = arcade.color.PHTHALO_GREEN
+BOX_SIZE = 25
 
 class Halves(arcade.Window):
     def __init__(self):
@@ -14,6 +15,11 @@ class Halves(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        for top in range(SCREEN_HEIGHT//BOX_SIZE+1):
+            for right in range(SCREEN_WIDTH//BOX_SIZE+1):
+                centerx = right * BOX_SIZE - (BOX_SIZE//2)
+                centery = top * BOX_SIZE - (BOX_SIZE//2)
+                arcade.draw_rectangle_outline(centerx, centery, BOX_SIZE, BOX_SIZE, arcade.color.BLACK)
 
     def on_key_press(self, key, modifiers):       
         if key == arcade.key.ESCAPE:
