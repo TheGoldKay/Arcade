ffad0d5c981049a0dfa7b21c0bc732ec52d424fb
GoldKay <jhonnatanwfc@live.com>
2024-12-18 22:08:13 -0300

    helper package

---
+++ helper/coordinates.py
@@ -0,0 +1,5 @@
+def centerTopLeft(top, left, height, width):
+    return (left + width // 2, top - height // 2)
+
+def centerTopRight(top, right, height, width):
+    return (right - width // 2, top - height // 2)
