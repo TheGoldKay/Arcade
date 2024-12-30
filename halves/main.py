6ce5a532366128e0e7aacef83e12f78b70de1d58
GoldKay <jhonnatanwfc@live.com>
2024-12-30 14:25:39 -0300

    const var fix's

--- halves/main.py
+++ halves/main.py
@@ -10,13 +10,16 @@ SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 400
 SCREEN_TITLE = "Halves"
 BACKGROUND_COLOR = arcade.color.PHTHALO_GREEN
+LINE_COLOR =(BACKGROUND_COLOR[0] + 50, BACKGROUND_COLOR[1] + 50, BACKGROUND_COLOR[2] + 50)
 BOX_SIZE = 25
+NWIDTH = SCREEN_WIDTH//BOX_SIZE
+NHEIGHT = SCREEN_HEIGHT//BOX_SIZE
 
 def middle():
-    cx = ((SCREEN_WIDTH//BOX_SIZE + 1) //  2) * BOX_SIZE - BOX_SIZE // 2
-    for y in range(1, SCREEN_HEIGHT//BOX_SIZE+1):
+    cx = ((NWIDTH + 1) //  2) * BOX_SIZE - BOX_SIZE // 2
+    for y in range(1, NHEIGHT+1):
         cy = y * BOX_SIZE - BOX_SIZE // 2
-        arcade.draw_rectangle_filled(cx, cy, BOX_SIZE, BOX_SIZE, arcade.color.BLACK)
+        arcade.draw_rectangle_filled(cx, cy, BOX_SIZE, BOX_SIZE, LINE_COLOR)
 
 class Halves(arcade.Window):
     def __init__(self):
@@ -27,8 +30,8 @@ class Halves(arcade.Window):
         arcade.start_render()
         
         middle()
-        for y in range(1, SCREEN_HEIGHT//BOX_SIZE+1):
-            for x in range(1, SCREEN_WIDTH//BOX_SIZE+1):
+        for y in range(1, NHEIGHT+1):
+            for x in range(1, NWIDTH+1):
                 top = y * BOX_SIZE
                 right = x * BOX_SIZE
                 centerx, centery = centerTopRight(top, right, BOX_SIZE, BOX_SIZE)
