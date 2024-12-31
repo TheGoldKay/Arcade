15f238989b27585bbe2500ddf1f6ebe38db85743
GoldKay <jhonnatanwfc@live.com>
2024-12-31 08:29:23 -0300

    ball init

--- halves/main.py
+++ halves/main.py
@@ -21,10 +21,21 @@ def middle():
         cy = y * BOX_SIZE - BOX_SIZE // 2
         arcade.draw_rectangle_filled(cx, cy, BOX_SIZE, BOX_SIZE, LINE_COLOR)
 
+class Ball:
+    def __init__(self):
+        self.cx = ((NWIDTH + 1) //  2) * BOX_SIZE - BOX_SIZE // 2
+        self.cy = ((NHEIGHT + 1) //  2) * BOX_SIZE - BOX_SIZE // 2
+    
+    def draw(self):
+        arcade.draw_circle_filled(self.cx, self.cy, BOX_SIZE // 2, arcade.color.RED)
+
 class Halves(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, center_window=True)
         arcade.set_background_color(BACKGROUND_COLOR)
+    
+    def setup(self):
+        self.ball = Ball()
 
     def on_draw(self):
         arcade.start_render()
@@ -36,6 +47,7 @@ class Halves(arcade.Window):
                 right = x * BOX_SIZE
                 centerx, centery = centerTopRight(top, right, BOX_SIZE, BOX_SIZE)
                 arcade.draw_rectangle_outline(centerx, centery, BOX_SIZE, BOX_SIZE, arcade.color.BLACK)
+        self.ball.draw()
 
         arcade.finish_render()
     def on_key_press(self, key, modifiers):       
@@ -44,6 +56,7 @@ class Halves(arcade.Window):
 
 def main():
     halves = Halves()
+    halves.setup()
     halves.run()
 
 if __name__ == "__main__":
